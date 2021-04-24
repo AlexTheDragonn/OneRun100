57ddbf09f9c9e398e213758768a5794ab71a0989
Alex <Alexthedragonjcraft@gmail.com>
2021-04-24 20:59:56 +0200

    forgot to add changes lol
    added everything

---
+++ Risk of Rain 2 Mod/Class1.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace AlexTheDragon
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.AlexTheDragon.TestMod", "My Mod's Title and if we see this exact name on Thunderstore we will deprecate your mod", "1.0.0")]
+    public class TestMod : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+        }
+    }
+}
